8903c3d97b81beb6b5f044400078a8b386347d41
WK <wsko@wise.co.kr>
2021-08-15 22:17:32 +0900

    cleanup

--- src/main/java/kr/wise/demo/pivotgrid/service/SalesDataService.java
+++ src/main/java/kr/wise/demo/pivotgrid/service/SalesDataService.java
@@ -1,10 +1,6 @@
 package kr.wise.demo.pivotgrid.service;
 
 import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +23,8 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import kr.wise.demo.pivotgrid.aggregator.DataAggregator;
 import kr.wise.demo.pivotgrid.impl.csv.CSVDataReader;
 import kr.wise.demo.pivotgrid.impl.csv.CSVDataReaderDataFrame;
-import kr.wise.demo.pivotgrid.model.AbstractSummaryContainer;
 import kr.wise.demo.pivotgrid.model.DataAggregation;
 import kr.wise.demo.pivotgrid.model.DataFrame;
-import kr.wise.demo.pivotgrid.model.DataGroup;
-import kr.wise.demo.pivotgrid.model.DataRow;
-import kr.wise.demo.pivotgrid.model.Paging;
 import kr.wise.demo.pivotgrid.param.FilterParam;
 import kr.wise.demo.pivotgrid.param.GroupParam;
 import kr.wise.demo.pivotgrid.param.PagingParam;
@@ -41,6 +32,7 @@ import kr.wise.demo.pivotgrid.param.SummaryParam;
 import kr.wise.demo.pivotgrid.repository.SalesDataRepository;
 import kr.wise.demo.pivotgrid.util.DataAggregationUtils;
 import kr.wise.demo.pivotgrid.util.ParamUtils;
+import kr.wise.demo.pivotgrid.util.PivotGridJsonUtils;
 
 @RestController
 public class SalesDataService {
@@ -127,12 +119,12 @@ public class SalesDataService {
                             pagingParam);
                 }
 
-                writeSummaryContainerToJson(gen, aggregation, null, "data", aggregation.getPaging(),
-                        pagingApplicable);
+                PivotGridJsonUtils.writeSummaryContainerToJson(gen, aggregation, null, "data",
+                        aggregation.getPaging(), pagingApplicable);
             }
             else {
                 log.debug("Simple data request invoked. skip: {}, take: {}", skip, take);
-                writeTabularDataToJson(gen, dataFrame, skip, take);
+                PivotGridJsonUtils.writeTabularDataToJson(gen, dataFrame, skip, take);
             }
         }
         catch (Exception e) {
@@ -173,90 +165,4 @@ public class SalesDataService {
         return true;
     }
 
-    private void writeSummaryContainerToJson(final JsonGenerator gen,
-            final AbstractSummaryContainer<?> summaryContainer, final String key,
-            final String childDataGroupArrayFieldName, final Paging paging, final boolean visibleOnly) throws IOException {
-        gen.writeStartObject();
-
-        if (key != null) {
-            gen.writeStringField("key", key);
-        }
-
-        gen.writeFieldName("summary");
-
-        gen.writeStartArray();
-        final List<BigDecimal> summary = summaryContainer.getSummary();
-        if (summary != null) {
-            for (BigDecimal value : summary) {
-                gen.writeNumber(value);
-            }
-        }
-        gen.writeEndArray();
-
-        if (paging != null) {
-            gen.writeFieldName("paging");
-            gen.writeStartObject();
-            gen.writeNumberField("offset", paging.getOffset());
-            gen.writeNumberField("limit", paging.getLimit());
-            gen.writeNumberField("count", paging.getCount());
-            gen.writeNumberField("total", paging.getTotal());
-            gen.writeEndObject();
-        }
-
-        gen.writeFieldName(childDataGroupArrayFieldName);
-        final List<DataGroup> childDataGroups = summaryContainer.getChildDataGroups(visibleOnly);
-        if (childDataGroups == null) {
-            gen.writeNull();
-        }
-        else {
-            gen.writeStartArray();
-            for (DataGroup childDataGroup : childDataGroups) {
-                writeSummaryContainerToJson(gen, childDataGroup, childDataGroup.getKey(), "items", null, visibleOnly);
-            }
-            gen.writeEndArray();
-        }
-
-        gen.writeEndObject();
-    }
-
-    private void writeTabularDataToJson(final JsonGenerator gen, final DataFrame dataFrame,
-            final int skip, final int take) throws IOException {
-        gen.writeStartArray();
-
-        final Iterator<DataRow> it = dataFrame.iterator();
-
-        if (skip > 0) {
-            for (int i = 0; i < skip && it.hasNext(); i++) {
-                it.next();
-            }
-        }
-
-        final String[] columnNames = dataFrame.getColumnNames();
-        int iterCount = 0;
-
-        while (it.hasNext()) {
-            final DataRow row = it.next();
-
-            if (take > 0 && ++iterCount > take) {
-                break;
-            }
-
-            gen.writeStartObject();
-
-            for (String columnName : columnNames) {
-                final String value = row.getStringValue(columnName);
-
-                if (NumberUtils.isParsable(value)) {
-                    gen.writeNumberField(columnName, new BigDecimal(value));
-                }
-                else {
-                    gen.writeStringField(columnName, value);
-                }
-            }
-
-            gen.writeEndObject();
-        }
-
-        gen.writeEndArray();
-    }
 }
---
+++ src/main/java/kr/wise/demo/pivotgrid/util/PivotGridJsonUtils.java
@@ -0,0 +1,109 @@
+package kr.wise.demo.pivotgrid.util;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import kr.wise.demo.pivotgrid.model.AbstractSummaryContainer;
+import kr.wise.demo.pivotgrid.model.DataFrame;
+import kr.wise.demo.pivotgrid.model.DataGroup;
+import kr.wise.demo.pivotgrid.model.DataRow;
+import kr.wise.demo.pivotgrid.model.Paging;
+
+public final class PivotGridJsonUtils {
+
+    private PivotGridJsonUtils() {
+    }
+
+    public static void writeSummaryContainerToJson(final JsonGenerator gen,
+            final AbstractSummaryContainer<?> summaryContainer, final String key,
+            final String childDataGroupArrayFieldName, final Paging paging, final boolean visibleOnly) throws IOException {
+        gen.writeStartObject();
+
+        if (key != null) {
+            gen.writeStringField("key", key);
+        }
+
+        gen.writeFieldName("summary");
+
+        gen.writeStartArray();
+        final List<BigDecimal> summary = summaryContainer.getSummary();
+        if (summary != null) {
+            for (BigDecimal value : summary) {
+                gen.writeNumber(value);
+            }
+        }
+        gen.writeEndArray();
+
+        if (paging != null) {
+            gen.writeFieldName("paging");
+            gen.writeStartObject();
+            gen.writeNumberField("offset", paging.getOffset());
+            gen.writeNumberField("limit", paging.getLimit());
+            gen.writeNumberField("count", paging.getCount());
+            gen.writeNumberField("total", paging.getTotal());
+            gen.writeEndObject();
+        }
+
+        gen.writeFieldName(childDataGroupArrayFieldName);
+        final List<DataGroup> childDataGroups = summaryContainer.getChildDataGroups(visibleOnly);
+        if (childDataGroups == null) {
+            gen.writeNull();
+        }
+        else {
+            gen.writeStartArray();
+            for (DataGroup childDataGroup : childDataGroups) {
+                writeSummaryContainerToJson(gen, childDataGroup, childDataGroup.getKey(), "items", null, visibleOnly);
+            }
+            gen.writeEndArray();
+        }
+
+        gen.writeEndObject();
+    }
+
+    public static void writeTabularDataToJson(final JsonGenerator gen, final DataFrame dataFrame,
+            final int skip, final int take) throws IOException {
+        gen.writeStartArray();
+
+        final Iterator<DataRow> it = dataFrame.iterator();
+
+        if (skip > 0) {
+            for (int i = 0; i < skip && it.hasNext(); i++) {
+                it.next();
+            }
+        }
+
+        final String[] columnNames = dataFrame.getColumnNames();
+        int iterCount = 0;
+
+        while (it.hasNext()) {
+            final DataRow row = it.next();
+
+            if (take > 0 && ++iterCount > take) {
+                break;
+            }
+
+            gen.writeStartObject();
+
+            for (String columnName : columnNames) {
+                final String value = row.getStringValue(columnName);
+
+                if (NumberUtils.isParsable(value)) {
+                    gen.writeNumberField(columnName, new BigDecimal(value));
+                }
+                else {
+                    gen.writeStringField(columnName, value);
+                }
+            }
+
+            gen.writeEndObject();
+        }
+
+        gen.writeEndArray();
+    }
+}
